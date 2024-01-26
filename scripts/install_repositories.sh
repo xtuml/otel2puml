94416f5de7d374eced2a41f10f21f59bfcbabe4f
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-01-26 10:57:51 +0000

    CDSTH-507 Testing several puml files and several methods for determining processes and maps

---
+++ scripts/install_repositories.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Fail if any part of this script fails
+set -e
+
+# test event generator
+cd /tmp/
+git clone git@github.com:SmartDCSITlimited/test-event-generator.git
+cd test-event-generator
+git fetch --all --tags
+git checkout main 
+pip install -r requirements.txt
+pip install .
+cd ..
+rm -f -r test-event-generator/
