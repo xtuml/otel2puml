af99b907a10edbfa6a45e88dd304ae8be4d836ef
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-02-08 13:19:26 +0000

    CDSTH-545 Updated install repositories to install branch version of test harness that is pip installable; updated devcontainer to 3.11 due to conflic with Test Harness repo

--- scripts/install_repositories.sh
+++ scripts/install_repositories.sh
@@ -8,7 +8,17 @@ cd /tmp/
 git clone git@github.com:SmartDCSITlimited/test-event-generator.git
 cd test-event-generator
 git fetch --all --tags
-git checkout main 
+git checkout v1.0.0 
+pip install -r requirements.txt
+pip install .
+cd ..
+rm -f -r test-event-generator/
+
+# test harness
+cd /tmp/
+git clone git@github.com:SmartDCSITlimited/test-harness.git
+cd test-harness
+git checkout CDSTH-545-Create-testing-input-pipeline-PUML-to-test-data 
 pip install -r requirements.txt
 pip install .
 cd ..
