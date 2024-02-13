4baa773f2789869d90fab439cda321e3201c4377
jack.hinton <jack.hinton@smartdcs.co.uk>
2024-02-13 16:16:51 +0000

    [CDSTH_549] Linting fixes

--- Jalergia2PUML/Jalergia2PUML.py
+++ Jalergia2PUML/Jalergia2PUML.py
@@ -1,71 +1,102 @@
+"""
+This module provides functions to extract nodes and edges from input data, 
+convert them into a NetworkX graph, and optionally write the graph to a file.
+
+Functions:
+- get_nodes: Extracts nodes from the given data and updates the node_reference, 
+    events_reference, and node_list.
+- get_edges: Extracts edges from the given data and populates the 
+    edge_list dictionary.
+- convert_to_networkx: Converts the given events reference and edge list 
+    into a NetworkX graph.
+
+Usage:
+1. Import the module: `import Jalergia2PUML`
+2. Call the functions as needed, providing the required arguments.
+3. Optionally, specify the `write` parameter in `convert_to_networkx` to write 
+    the graph to a file.
+
+"""
+
 import re
 import networkx as nx
 
 
-def getNodes(
-    data: str, nodeReference: dict, eventsReference: dict, nodeList: list
+def get_nodes(
+    data: str, node_reference: dict, events_reference: dict, node_list: list
 ):
     """
-    Extracts nodes from the given data and updates the nodeReference, eventsReference, and nodeList.
+    Extracts nodes from the given data and updates the node_reference, events_reference, and node_list.
 
     Args:
         data (str): The input data containing node information.
-        nodeReference (dict): A dictionary to store the node references.
-        eventsReference (dict): A dictionary to store the event references.
-        nodeList (list): A list to store the node names.
+        node_reference (dict): A dictionary to store the node references.
+        events_reference (dict): A dictionary to store the event references.
+        node_list (list): A list to store the node names.
 
     Returns:
-        tuple: A tuple containing the updated nodeReference, eventsReference, and nodeList.
+        tuple: A tuple containing the updated node_reference, events_reference, and node_list.
 
     These returns are formatted as following:
-    nodeReference   - {'event': 'node', ..}
-    eventsReference - {'node': ['event1', 'event2', ..], ..}
-    nodeList        - ["node1", "node2", ..]
+    node_reference   - {'event': 'node', ..}
+    events_reference - {'node': ['event1', 'event2', ..], ..}
+    node_list        - ["node1", "node2", ..]
     """
     for node in re.findall(r"(.*?) \[shape=\".*?\",label=\"(.*?)\"", data):
-        if node[1] not in nodeReference:
-            nodeReference[node[1]] = [node[0]]
-            eventsReference[node[0]] = node[1]
-            nodeList.append(node[1])
+        if node[1] not in node_reference:
+            node_reference[node[1]] = [node[0]]
+            events_reference[node[0]] = node[1]
+            node_list.append(node[1])
         else:
-            nodeReference[node[1]].append([node[0]])
-            eventsReference[node[0]] = node[1]
+            node_reference[node[1]].append([node[0]])
+            events_reference[node[0]] = node[1]
 
-    return nodeReference, eventsReference, nodeList
+    return node_reference, events_reference, node_list
 
 
-def getEdges(data: str, edgeList: dict):
+def get_edges(data: str, edge_list: dict):
     """
-    Extracts edges from the given data and populates the edgeList dictionary.
+    Extracts edges from the given data and populates the edge_list dictionary.
 
     Args:
         data (str): The input data containing edge information.
-        edgeList (dict): The dictionary to store the extracted edges.
+        edge_list (dict): The dictionary to store the extracted edges.
 
     Returns:
-        dict: The updated edgeList dictionary.
+        dict: The updated edge_list dictionary.
 
-    edgeList has the following format; {("edgestart", "edgeend"): {"weight":5, ..}, ..}
+    edge_list has the following format; {("edgestart", "edgeend"): {"weight":5, ..}, ..}
     """
     for edge in re.findall(r"(.*?)->(.*?) \[label=\"(.*?)\"", data):
         if edge[0] != "__start0 ":
             if edge[2] != "":
-                edgeList[(edge[0], edge[1])] = {"weight": str(edge[2])}
+                edge_list[(edge[0], edge[1])] = {"weight": str(edge[2])}
             else:
-                edgeList[(edge[0], edge[1])] = {}
+                edge_list[(edge[0], edge[1])] = {}
 
-    return edgeList
+    return edge_list
 
 
-def convertToNetworkX(eventsReference: dict, edgeList: dict, write=False):
+def convert_to_networkx(events_reference: dict, edge_list: dict, write=False):
+    """
+    Converts the given events reference and edge list into a NetworkX graph.
+
+    Args:
+        events_reference (dict): A dictionary containing the events reference.
+        edge_list (dict): A dictionary containing the edge list.
+        write (bool, optional): Indicates whether to write the graph to a file. Defaults to False.
+
+    Returns:
+        graph (nx.MultiDiGraph): The converted NetworkX graph.
+    """
 
     graph = nx.MultiDiGraph()
 
-    for item in eventsReference:
-        graph.add_node(item, label=eventsReference[item])
-    for item in edgeList:
+    for item in events_reference:
+        graph.add_node(item, label=events_reference[item])
+    for item in edge_list:
         graph.add_edge(
-            str(item[0]), str(item[1]), label=str(edgeList[item]["weight"])
+            str(item[0]), str(item[1]), label=str(edge_list[item]["weight"])
         )
     if write:
         graph.edges(data=True)
@@ -76,17 +107,17 @@ def convertToNetworkX(eventsReference: dict, edgeList: dict, write=False):
 
 if __name__ == "__main__":
 
-    nodeReference = {}
-    eventsReference = {}
-    nodeList = []
-    edgeList = {}
+    node_reference = {}
+    events_reference = {}
+    node_list = []
+    edge_list = {}
 
     with open("Jalergia2PUML/jAlergiaModel.dot", "r") as file:
         data = file.read()
 
-    nodeReference, eventsReference, nodeList = getNodes(
-        data, nodeReference, eventsReference, nodeList
+    node_reference, events_reference, node_list = get_nodes(
+        data, node_reference, events_reference, node_list
     )
-    edgeList = getEdges(data, edgeList)
+    edge_list = get_edges(data, edge_list)
 
-    convertToNetworkX(eventsReference, edgeList, write=True)
+    convert_to_networkx(events_reference, edge_list, write=True)
