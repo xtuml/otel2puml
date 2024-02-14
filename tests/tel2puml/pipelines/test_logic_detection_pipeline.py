5751ca0522101c7d1f1ff4be022bd35b02ac5304
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-02-14 12:05:43 +0000

    CDSTH-547 Tests for data from events sets and conditional probability matrix

--- tests/tel2puml/pipelines/test_logic_detection_pipeline.py
+++ tests/tel2puml/pipelines/test_logic_detection_pipeline.py
@@ -1,6 +1,7 @@
 """Test the logic detection pipeline.
 """
-from test_harness.utils import check_dict_equivalency
+from datetime import datetime, timedelta
+
 from tel2puml.pipelines.logic_detection_pipeline import Event
 
 
@@ -38,14 +39,9 @@ class TestEvent:
         ):
             event.add_new_edge_to_edge_counts_per_data_point(edge_tuple)
             assert edge_tuple in event.edge_counts_per_data_point
-            check_dict_equivalency(
-                event.edge_counts_per_data_point[edge_tuple],
-                {
-                    "data_count": 0,
-                    "total_count": 0,
-                    "index": i,
-                }
-            )
+            assert (
+                event.edge_counts_per_data_point[edge_tuple]
+            )["index"] == i
 
     @staticmethod
     def test_update_conditional_count_matrix():
@@ -75,3 +71,224 @@ class TestEvent:
             for j in [index_ab, index_bc]:
                 to_check_list[i][j] += 1
         assert event.conditional_count_matrix.tolist() == to_check_list
+
+    @staticmethod
+    def test_update_with_edge_tuple_and_data_point_edges():
+        """Tests for method update_with_edge_tuple_and_data_point_edges"""
+        event = Event("A")
+        edge_tuple = ("A", "B")
+        data_point_edges = set()
+        event.update_with_edge_tuple_and_data_point_edges(
+            edge_tuple,
+            data_point_edges,
+        )
+        assert edge_tuple in event.edge_counts_per_data_point
+        assert event.edge_counts_per_data_point[edge_tuple]["index"] == 0
+        assert event.conditional_count_matrix.tolist() == [[0]]
+        assert data_point_edges == {edge_tuple}
+
+    @staticmethod
+    def test_update_with_data_point():
+        """Tests for method update_with_data_point"""
+        event = Event("A")
+        assert event._update_since_conditional_probability_matrix is False
+        edge_tuples = [("A", "B"), ("B", "C"), ("C", "D")]
+        event.update_with_data_point(edge_tuples)
+        assert event.conditional_count_matrix.tolist() == [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        for i, edge_tuple in enumerate(edge_tuples):
+            assert edge_tuple in event.edge_counts_per_data_point
+            assert event.edge_counts_per_data_point[edge_tuple]["index"] == i
+        assert event._update_since_conditional_probability_matrix is True
+
+    @staticmethod
+    def test_conditional_probability_matrix():
+        """Tests for property conditional_probability_matrix"""
+        event = Event("A")
+        assert event.conditional_probability_matrix is None
+        edge_tuples = [("A", "B"), ("B", "C"), ("C", "D")]
+        event.update_with_data_point(edge_tuples)
+        assert event.conditional_probability_matrix.tolist() == [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        event.update_with_data_point(edge_tuples[:2])
+        assert event.conditional_probability_matrix.tolist() == [
+            [1, 1, 1],
+            [1, 1, 1],
+            [0.5, 0.5, 1],
+        ]
+        event.update_with_data_point(edge_tuples[:1])
+        assert event.conditional_probability_matrix.tolist() == [
+            [1, 1, 1],
+            [2/3, 1, 1],
+            [1/3, 0.5, 1],
+        ]
+
+    @staticmethod
+    def test_update_event_sets():
+        """Tests for method update_edge_sets"""
+        event = Event("A")
+        events = ["B", "C", "D"]
+        # check that the event sets are updated
+        event.update_event_sets(events)
+        assert event.event_sets == {frozenset(events)}
+        # check that the event sets are not updated
+        event.update_event_sets(events)
+        assert event.event_sets == {frozenset(events)}
+        # check that the event sets are not updated if events are reversed
+        event.update_event_sets(["D", "C", "B"])
+        assert event.event_sets == {frozenset(events)}
+        # check that the event sets are updated
+        event.update_event_sets(["B", "C"])
+        assert event.event_sets == {frozenset(events), frozenset(["B", "C"])}
+
+    @staticmethod
+    def test_create_data_from_event_sequence():
+        event_sequence = ["A", "B", "C", "D"]
+        case_id = "case_1"
+        start_time = datetime(2021, 1, 1, 0, 0, 0)
+        data = Event.create_data_from_event_sequence(
+            event_sequence,
+            case_id,
+            start_time
+        )
+        for i, event_data in enumerate(data):
+            assert event_data["case_id"] == case_id
+            assert event_data["activity"] == event_sequence[i]
+            assert event_data["timestamp"] == start_time + timedelta(seconds=i)
+
+    @staticmethod
+    def test_create_augmented_data_from_event_set():
+        event = Event("A")
+        event_set = frozenset(["B", "C"])
+        data = list(event.created_augemented_data_from_event_set(
+            event_set,
+        ))
+        assert len(data) == 6
+        expected_sequences = [
+            "ABC",
+            "ACB",
+        ]
+        expected_sequences.remove("".join(
+            [event_data["activity"] for event_data in data[:3]]
+        ))
+        expected_sequences.remove("".join(
+            [event_data["activity"] for event_data in data[3:]]
+        ))
+        assert len(expected_sequences) == 0
+        first_seq_case_ids = set(
+            [event_data["case_id"] for event_data in data[:3]]
+        )
+        second_seq_case_ids = set(
+            [event_data["case_id"] for event_data in data[3:]]
+        )
+        assert len(first_seq_case_ids) == 1
+        assert len(second_seq_case_ids) == 1
+        assert first_seq_case_ids != second_seq_case_ids
+
+    @staticmethod
+    def test_create_augmented_data_from_event_sets():
+        event = Event("A")
+        event.event_sets = {
+            frozenset(["B", "C"]),
+            frozenset(["D", "E"]),
+        }
+        data = list(event.create_augmented_data_from_event_sets())
+        assert len(data) == 12
+        expected_sequences = [
+            "ABC",
+            "ACB",
+            "ADE",
+            "AED",
+        ]
+        for i in range(4):
+            expected_sequences.remove("".join(
+                [event_data["activity"] for event_data in data[i*3:i*3+3]]
+            ))
+        assert len(expected_sequences) == 0
+
+    @staticmethod
+    def test_calculate_process_tree_from_event_sets_xor_ands():
+        event = Event("A")
+        event.event_sets = {
+            frozenset(["B", "C"]),
+            frozenset(["D", "E"]),
+        }
+        process_tree = event.calculate_process_tree_from_event_sets()
+        first_children = process_tree.children
+        assert len(first_children) == 2
+        start_event = first_children[0]
+        next_operator = first_children[1]
+        assert start_event.label == "A"
+        assert next_operator.operator.name == "XOR"
+        second_children = next_operator.children
+        assert len(second_children) == 2
+        first_operator = second_children[0]
+        second_operator = second_children[1]
+        assert first_operator.operator.name == "PARALLEL"
+        assert second_operator.operator.name == "PARALLEL"
+        first_operator_children = first_operator.children
+        assert len(first_operator_children) == 2
+        second_operator_children = second_operator.children
+        assert len(second_operator_children) == 2
+        children_labels = []
+        for operator_children in [
+            first_operator_children, second_operator_children
+        ]:
+            operator_child_labels = []
+            for child in operator_children:
+                assert len(child.children) == 0
+                operator_child_labels.append(child.label)
+            children_labels.append(frozenset(sorted(operator_child_labels)))
+        for to_remove in [
+            frozenset(sorted(["B", "C"])),
+            frozenset(sorted(["D", "E"]))
+        ]:
+            children_labels.remove(to_remove)
+        assert len(children_labels) == 0
+
+    @staticmethod
+    def test_calculate_process_tree_from_event_sets_or():
+        event = Event("A")
+        event.event_sets = {
+            frozenset(["B", "C"]),
+            frozenset(["B"]),
+            frozenset(["C"]),
+        }
+        process_tree = event.calculate_process_tree_from_event_sets()
+        first_children = process_tree.children
+        assert len(first_children) == 2
+        start_event = first_children[0]
+        next_operator = first_children[1]
+        assert start_event.label == "A"
+        assert next_operator.operator.name == "PARALLEL"
+        second_children = next_operator.children
+        assert len(second_children) == 2
+        first_operator = second_children[0]
+        second_operator = second_children[1]
+        assert first_operator.operator.name == "XOR"
+        assert second_operator.operator.name == "XOR"
+        first_operator_children = first_operator.children
+        assert len(first_operator_children) == 2
+        second_operator_children = second_operator.children
+        assert len(second_operator_children) == 2
+        children_labels = []
+        for operator_children in [
+            first_operator_children, second_operator_children
+        ]:
+            operator_child_labels = []
+            for child in operator_children:
+                assert len(child.children) == 0
+                operator_child_labels.append(str(child))
+            children_labels.append(frozenset(sorted(operator_child_labels)))
+        for to_remove in [
+            frozenset(sorted(["B", "tau"])),
+            frozenset(sorted(["C", "tau"])),
+        ]:
+            children_labels.remove(to_remove)
+        assert len(children_labels) == 0
