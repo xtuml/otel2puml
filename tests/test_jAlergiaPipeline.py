76afc9157ef627f522dc74a4fcf040de374170dd
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-03-19 19:32:38 +0000

    CDSTH-564 Created pipeline to get a network x graph from a stream of audit event sequences using jAlregia

--- tests/test_jAlergiaPipeline.py
+++ tests/test_jAlergiaPipeline.py
@@ -1,9 +1,21 @@
 """
 Unit tests for the JAlergiaPipeline class.
 """
-
 import unittest
-from tel2puml.jAlergiaPipeline import process_puml_into_graphs
+
+from networkx import MultiDiGraph
+
+from tel2puml.jAlergiaPipeline import (
+    process_puml_into_graphs,
+    markov_lines_to_network_x,
+    audit_event_sequences_to_network_x
+)
+from tel2puml.read_uml_file import (
+    get_markov_sequence_lines_from_audit_event_stream
+)
+from tel2puml.pipelines.data_creation import (
+    generate_test_data_event_sequences_from_puml,
+)
 
 
 class TestJAlergiaPipeline(unittest.TestCase):
@@ -53,6 +65,56 @@ class TestJAlergiaPipeline(unittest.TestCase):
         self.assertEqual(
             "MultiDiGraph with 2 nodes and 1 edges", str(graphs[0][0])
         )
+
+
+class TestMarkovPipelineTest:
+    """Test the `markov_lines_to_network_x` and
+    `audit_event_sequences_to_network_x` functions
+    """
+    @staticmethod
+    def check_graph_and_refs(
+        graph: MultiDiGraph, references: dict[str, dict]
+    ) -> None:
+        """Check the graph for correctness using the node references
+
+        :param graph: The graph to check
+        :type graph: :class:`MultiDiGraph`
+        :param references: The references to use
+        :type references: `dict`[`str`, `dict`]
+        """
+        assert graph.number_of_nodes() == 4
+        assert graph.number_of_edges() == 3
+        node_references = references["node_reference"]
+        expected_edges = [
+            (node_references["A"][0], node_references["D"][0]),
+            (node_references["A"][0], node_references["B"][0]),
+            (node_references["B"][0], node_references["C"][0]),
+        ]
+        for edge in graph.edges():
+            assert edge in expected_edges
+            expected_edges.remove(edge)
+        assert len(expected_edges) == 0
+
+    def test_markov_lines_to_network_x(self) -> None:
+        """Test the `markov_lines_to_network_x` function.
+        """
+        event_sequences = generate_test_data_event_sequences_from_puml(
+            "puml_files/simple_test.puml"
+        )
+        markov_lines = get_markov_sequence_lines_from_audit_event_stream(
+            event_sequences
+        )
+        graph, references = markov_lines_to_network_x(markov_lines)
+        self.check_graph_and_refs(graph, references)
+
+    def test_audit_event_sequences_to_network_x(self) -> None:
+        """Test the `audit_event_sequences_to_network_x` function.
+        """
+        event_sequences = generate_test_data_event_sequences_from_puml(
+            "puml_files/simple_test.puml"
+        )
+        graph, references = audit_event_sequences_to_network_x(event_sequences)
+        self.check_graph_and_refs(graph, references)
 
 
 if __name__ == "__main__":
