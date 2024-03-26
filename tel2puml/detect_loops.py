7b49e16e9d1020640144f2858dd6e185695ae595
Ravinda Gunaratne <ravinda.gunaratne@smartdcs.co.uk>
2024-03-26 17:07:54 +0000

    CDSTH-646 Use data creation pipeline in unit tests (no logic implemented)

--- tel2puml/detect_loops.py
+++ tel2puml/detect_loops.py
@@ -3,10 +3,25 @@ from networkx import DiGraph, simple_cycles
 
 class Loop:
     
-    def __init__(self):
+    def __init__(self, nodes: list[str]):
+        self.nodes = nodes
         self.sub_loops: list[Loop] = []
 
 
-def detect_loops(graph: DiGraph) -> list[Loop]:
-    _ = simple_cycles(graph)
-    return [Loop()]
+def detect_loops(
+        graph: DiGraph, 
+        references: dict[str, dict]
+) -> list[Loop]:
+    result = list(simple_cycles(graph))
+    result_with_ref = [
+        _update_with_references(res, references) for res in result
+    ]
+    result_with_ref.sort(key=lambda x: len(x))
+
+    return [Loop(res) for res in result_with_ref]
+
+def _update_with_references(
+        loop: list, 
+        references: dict[str, dict]
+) -> None:
+    return [references["event_reference"][label] for label in loop]
--- tests/test_detect_loops.py
+++ tests/test_detect_loops.py
@@ -1,16 +1,22 @@
 from tel2puml.detect_loops import Loop, detect_loops
+from tel2puml.jAlergiaPipeline import audit_event_sequences_to_network_x
+from tel2puml.pipelines.data_creation import (
+    generate_test_data_event_sequences_from_puml,
+)
 
 
 class TestLoop():
     
     @staticmethod
     def test_init():
-        loop = Loop()
+        loop = Loop(["A"])
         assert isinstance(loop.sub_loops, list)
+        assert loop.nodes == ["A"]
 
 
-def test_detect_loops():
-    graph = NotImplemented
-    loops = detect_loops(graph)
-    loop, = loops
-    assert isinstance(loop, Loop)
+def test_detect_loops_from_puml():
+    event_sequences = generate_test_data_event_sequences_from_puml(
+            "puml_files/loop_loop_a.puml"
+        )
+    graph, references = audit_event_sequences_to_network_x(event_sequences)
+    loops = detect_loops(graph, references)
