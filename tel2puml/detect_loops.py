f95402f68c7dc1b9cf09f1f4b86521dc753bec19
Ravinda Gunaratne <ravinda.gunaratne@smartdcs.co.uk>
2024-03-27 11:39:50 +0000

    CDSTH-646 Add logic to detect loops and subloops, unit tests

--- tel2puml/detect_loops.py
+++ tel2puml/detect_loops.py
@@ -1,27 +1,79 @@
+from typing import Self
+
 from networkx import DiGraph, simple_cycles
 
 
 class Loop:
-    
+
     def __init__(self, nodes: list[str]):
         self.nodes = nodes
         self.sub_loops: list[Loop] = []
 
+    def __len__(self) -> int:
+        return len(self.nodes)
+
+    def get_node_cycles(self) -> list[str]:
+        index = 0
+        cycles = []
+        while index < len(self.nodes):
+            cycles.append(self.nodes[index:] + self.nodes[:index])
+            index += 1
+
+        return cycles
+
+    def check_subloop(self, other: Self) -> bool:
+        if other.nodes in self.get_node_cycles():
+            return False
+        for sublist in self.get_sublist_of_length(self.nodes, len(other)):
+            if sublist == other.nodes:
+                return True
+        return False
+
+    @staticmethod
+    def get_sublist_of_length(
+            list: list[str],
+            length: int
+    ) -> list[list[str]]:
+        non_overlap = [
+            list[i:i+length]
+            for i in range(len(list)-length+1)
+        ]
+        overlap = [
+            list[-i:] + list[:-i+length]
+            for i in range(length-1, 0, -1)
+        ]
+        return non_overlap + overlap
+
+    def add_subloop(self, sub_loop: Self) -> None:
+        self.sub_loops.append(sub_loop)
+
 
 def detect_loops(
-        graph: DiGraph, 
+        graph: DiGraph,
         references: dict[str, dict]
 ) -> list[Loop]:
-    result = list(simple_cycles(graph))
-    result_with_ref = [
-        _update_with_references(res, references) for res in result
+    loops_with_ref = [
+        _update_with_references(loop_list, references)
+        for loop_list in simple_cycles(graph)
     ]
-    result_with_ref.sort(key=lambda x: len(x))
+    loops_with_ref.sort(key=lambda x: len(x))
+    loops = [Loop(res) for res in loops_with_ref]
+    return _update_subloops(loops)
 
-    return [Loop(res) for res in result_with_ref]
 
 def _update_with_references(
-        loop: list, 
+        loop_list: list,
         references: dict[str, dict]
 ) -> None:
-    return [references["event_reference"][label] for label in loop]
+    return [references["event_reference"][label] for label in loop_list]
+
+
+def _update_subloops(loops: list[Loop]) -> None:
+    subloop_indices = []
+    for loop in loops:
+        for sub_loop in loops:
+            if loop.check_subloop(sub_loop):
+                loop.add_subloop(sub_loop)
+                subloop_indices.append(loops.index(sub_loop))
+
+    return [loop for i, loop in enumerate(loops) if i not in subloop_indices]
--- tests/test_detect_loops.py
+++ tests/test_detect_loops.py
@@ -6,17 +6,103 @@ from tel2puml.pipelines.data_creation import (
 
 
 class TestLoop():
-    
+
     @staticmethod
     def test_init():
         loop = Loop(["A"])
         assert isinstance(loop.sub_loops, list)
         assert loop.nodes == ["A"]
 
+    @staticmethod
+    def test_get_node_cycles():
+        loop = Loop(["A"])
+        assert loop.get_node_cycles() == [["A"]]
+
+        loop = Loop(["A", "B"])
+        assert loop.get_node_cycles() == [
+            ["A", "B"],
+            ["B", "A"]
+        ]
+
+        loop = Loop(["A", "B", "C"])
+        assert loop.get_node_cycles() == [
+            ["A", "B", "C"],
+            ["B", "C", "A"],
+            ["C", "A", "B"]
+        ]
+
+    @staticmethod
+    def test_check_subloop():
+        loop = Loop(["A", "B", "C", "D"])
+
+        other = Loop(["A", "B", "C", "D"])
+        assert not loop.check_subloop(other)
+
+        other = Loop(["B"])
+        assert loop.check_subloop(other)
+
+        other = Loop(["A", "B"])
+        assert loop.check_subloop(other)
+
+        other = Loop(["C", "D"])
+        assert loop.check_subloop(other)
+
+        other = Loop(["D", "A"])
+        assert loop.check_subloop(other)
+
+        other = Loop(["B", "D"])
+        assert not loop.check_subloop(other)
+
+        other = Loop(["B", "D", "C"])
+        assert not loop.check_subloop(other)
+
+    @staticmethod
+    def test_get_sublist_of_length():
+        loop = Loop(["A"])
+        assert loop.get_sublist_of_length(["A"], 1) == [["A"]]
+
+        loop = Loop(["A", "B"])
+        assert loop.get_sublist_of_length(["A", "B"], 1) == [["A"], ["B"]]
+        assert loop.get_sublist_of_length(["A", "B"], 2) == [
+            ["A", "B"],
+            ["B", "A"]
+        ]
+
+        loop = Loop(["A", "B", "C", "D"])
+        assert loop.get_sublist_of_length(["A", "B", "C", "D"], 1) == [
+            ["A"], ["B"], ["C"], ["D"]
+        ]
+        assert loop.get_sublist_of_length(["A", "B", "C", "D"], 2) == [
+            ["A", "B"], ["B", "C"], ["C", "D"], ["D", "A"]
+        ]
+        assert loop.get_sublist_of_length(["A", "B", "C", "D"], 3) == [
+            ["A", "B", "C"], ["B", "C", "D"], ["C", "D", "A"], ["D", "A", "B"]
+        ]
+        assert loop.get_sublist_of_length(["A", "B", "C", "D"], 4) == [
+            ["A", "B", "C", "D"],
+            ["B", "C", "D", "A"],
+            ["C", "D", "A", "B"],
+            ["D", "A", "B", "C"],
+        ]
+
+    @staticmethod
+    def test_add_subloop():
+        loop = Loop(["A", "B", "C"])
+        sub_loop = Loop(["B"])
+
+        assert loop.sub_loops == []
+        loop.add_subloop(sub_loop)
+        assert loop.sub_loops == [sub_loop]
+
 
 def test_detect_loops_from_puml():
     event_sequences = generate_test_data_event_sequences_from_puml(
-            "puml_files/loop_loop_a.puml"
-        )
+        "puml_files/loop_loop_a.puml"
+    )
     graph, references = audit_event_sequences_to_network_x(event_sequences)
     loops = detect_loops(graph, references)
+    loop, = loops
+    assert ["B", "C", "D"] in loop.get_node_cycles()
+    assert len(loop.sub_loops) == 1
+    sub_loop, = loop.sub_loops
+    assert sub_loop.nodes == ["C"]
