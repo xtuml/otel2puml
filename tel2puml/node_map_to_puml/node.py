c5cbea36585f73cad059cd836f699c2d2a7cee26
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-03-27 13:59:50 +0000

    CDSTH-649 Updated Node class to be used as nodes in a networkx graph. Create functionality to create a networkx graph of Node's and mirror the connections of a markov chain graph and add logic details and connections to the Nodes

--- tel2puml/node_map_to_puml/node.py
+++ tel2puml/node_map_to_puml/node.py
@@ -4,6 +4,7 @@ from typing import Literal
 from uuid import uuid4
 import logging
 
+from networkx import DiGraph
 from pm4py import ProcessTree
 from pm4py.objects.process_tree.obj import Operator
 
@@ -67,6 +68,12 @@ class Node:
         self.is_stub = is_stub
 
         self.branch_enum = ["AND", "OR", "XOR", "LOOP", "BRANCH"]
+
+    def __repr__(self) -> str:
+        return self.uid + ":" + (self.event_type or self.operator or "None")
+
+    def __hash__(self) -> int:
+        return hash(self.uid)
 
     def update_node_list_with_nodes(
         self, nodes: list["Node"], direction: Literal["incoming", "outgoing"]
@@ -298,3 +305,95 @@ def load_logic_tree_into_nodes(
     """
     for node in nodes:
         node.load_logic_into_list(logic_gate_tree, direction)
+
+
+def create_networkx_graph_of_nodes_from_markov_graph(
+    markov_graph: DiGraph,
+    node_event_references: dict[str, str],
+) -> tuple[DiGraph, dict[str, list[Node]]]:
+    """Creates a NetworkX graph of nodes from a Markov graph.
+
+    :param markov_graph: The Markov graph to create the NetworkX graph from.
+    :type markov_graph: :class:`DiGraph`
+    :param node_event_references: The node event references.
+    :type node_event_references: `dict`[`str`, `str`]
+    :return: A tuple containing the NetworkX graph and the event node
+    reference.
+    :rtype: `tuple`[:class:`DiGraph`, `dict`[`str`, `list`[:class:`Node`]]]
+    """
+    networkx_graph = DiGraph()
+    uid_to_node = {}
+    for node in markov_graph.nodes:
+        node_class = Node(
+            uid=node,
+            event_type=node_event_references[node],
+        )
+        networkx_graph.add_node(node_class)
+        uid_to_node[node] = node_class
+    for node_class in networkx_graph.nodes:
+        edges_to_node = markov_graph.in_edges(node_class.uid)
+        for edge in edges_to_node:
+            networkx_graph.add_edge(
+                uid_to_node[edge[0]],
+                uid_to_node[edge[1]],
+            )
+            node_class.incoming.append(uid_to_node[edge[0]])
+        edges_from_node = markov_graph.out_edges(node_class.uid)
+        for edge in edges_from_node:
+            node_class.outgoing.append(uid_to_node[edge[1]])
+    event_node_ref = create_event_node_ref(networkx_graph)
+    return networkx_graph, event_node_ref
+
+
+def create_event_node_ref(
+    node_class_network_x_graph: DiGraph,
+) -> dict[str, list[Node]]:
+    """Creates an event node reference.
+
+    :param node_class_network_x_graph: The node class NetworkX graph.
+    :type node_class_network_x_graph: :class:`DiGraph`
+    :return: The event node reference.
+    :rtype: `dict`[`str`, `list`[:class:`Node`]]
+    """
+    event_node_ref = {}
+    for node in node_class_network_x_graph.nodes:
+        if node.event_type is None:
+            raise ValueError(
+                f"Node event type is not set for node with uid {node.uid}"
+            )
+        if node.event_type not in event_node_ref:
+            event_node_ref[node.event_type] = []
+        event_node_ref[node.event_type].append(node)
+    return event_node_ref
+
+
+def merge_markov_without_loops_and_logic_detection_analysis(
+    markov_graph_ref_pair: tuple[DiGraph, dict[str, str]],
+    incoming_logic_events: dict[str, Event],
+    outgoing_logic_events: dict[str, Event],
+) -> DiGraph:
+    """Merges a Markov graph without loops and logic detection analysis.
+
+    :param markov_graph_ref_pair: The Markov graph reference pair.
+    :type markov_graph_ref_pair: `tuple`[:class:`DiGraph`,
+    `dict`[`str`, `str`]]
+    :param incoming_logic_events: The incoming logic events.
+    :type incoming_logic_events: `dict`[`str`, :class:`Event`]
+    :param outgoing_logic_events: The outgoing logic events.
+    :type outgoing_logic_events: `dict`[`str`, :class:`Event`]
+    :return: The NetworkX graph of :class:`Node`'s.
+    :rtype: :class:`DiGraph`
+    """
+    markov_graph, node_event_references = markov_graph_ref_pair
+    node_class_graph, event_node_map = (
+        create_networkx_graph_of_nodes_from_markov_graph(
+            markov_graph, node_event_references
+        )
+    )
+    load_all_logic_trees_into_nodes(
+        incoming_logic_events, event_node_map, "incoming"
+    )
+    load_all_logic_trees_into_nodes(
+        outgoing_logic_events, event_node_map, "outgoing"
+    )
+    return node_class_graph
