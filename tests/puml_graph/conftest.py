99d6acaecbabde01ed2c39926b8a8ce23a281532
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-04-08 15:52:23 +0000

    CDSTH-684 Updated configuration file for puml_graph to contain expected output graphs for checking

--- tests/puml_graph/conftest.py
+++ tests/puml_graph/conftest.py
@@ -3,7 +3,7 @@
 import pytest
 
 from tel2puml.puml_graph.graph import PUMLGraph, PUMLEventNode
-from tel2puml.tel2puml_types import PUMLOperator
+from tel2puml.tel2puml_types import PUMLOperator, PUMLEvent
 from tel2puml.detect_loops import Loop
 
 
@@ -106,3 +106,65 @@ def loop_2(subloop: Loop) -> Loop:
     loop.add_edge_to_remove(("q7", "q5"))
     loop.sub_loops.append(subloop)
     return loop
+
+
+@pytest.fixture
+def expected_puml_graph_post_loop_insertion() -> PUMLGraph:
+    """Return a PUMLGraph instance after loop insertion.
+    """
+    graph = PUMLGraph()
+    A = graph.create_event_node(
+        "A", parent_graph_node="q0"
+    )
+    F = graph.create_event_node(
+        "F", parent_graph_node="q8"
+    )
+    LOOP1 = graph.create_event_node(
+        "LOOP", event_types=PUMLEvent.LOOP
+    )
+    LOOP2 = graph.create_event_node(
+        "LOOP", event_types=PUMLEvent.LOOP
+    )
+    LOOP3 = graph.create_event_node(
+        "LOOP", event_types=PUMLEvent.LOOP
+    )
+    and_start, and_end = graph.create_operator_node_pair(PUMLOperator.AND)
+    graph.add_edge(A, and_start)
+    graph.add_edge(and_end, F)
+    graph.add_edge(and_start, LOOP1)
+    graph.add_edge(and_start, LOOP2)
+    graph.add_edge(and_start, LOOP3)
+    graph.add_edge(LOOP1, and_end)
+    graph.add_edge(LOOP2, and_end)
+    graph.add_edge(LOOP3, and_end)
+    return graph
+
+
+@pytest.fixture
+def expected_loop_1_graph() -> PUMLGraph:
+    graph = PUMLGraph()
+    xor_start, xor_end = graph.create_operator_node_pair(PUMLOperator.XOR)
+    B = graph.create_event_node("B", parent_graph_node="q1")
+    C = graph.create_event_node("C", parent_graph_node="q2")
+    D = graph.create_event_node("D", parent_graph_node="q3")
+    E = graph.create_event_node("E", parent_graph_node="q4")
+    graph.add_edge(xor_start, B)
+    graph.add_edge(xor_start, C)
+    graph.add_edge(B, D)
+    graph.add_edge(C, E)
+    graph.add_edge(D, xor_end)
+    graph.add_edge(E, xor_end)
+    return graph
+
+
+@pytest.fixture
+def expected_loop_2_graph() -> PUMLGraph:
+    graph = PUMLGraph()
+    G = graph.create_event_node("G", parent_graph_node="q5")
+    LOOP = graph.create_event_node(
+        "LOOP", event_types=PUMLEvent.LOOP
+    )
+    INODE = graph.create_event_node("I", parent_graph_node="q7")
+    graph.add_edge(G, LOOP)
+    graph.add_edge(LOOP, INODE)
+    return graph
