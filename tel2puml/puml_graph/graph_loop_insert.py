d18ca1713626a504ce477ff342492c96f5e6630b
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-04-08 15:51:18 +0000

    CDSTH-684 update insert_loop and remove defunct function

--- tel2puml/puml_graph/graph_loop_insert.py
+++ tel2puml/puml_graph/graph_loop_insert.py
@@ -1,10 +1,11 @@
 from tel2puml.detect_loops import Loop
 from tel2puml.puml_graph.graph import (
-    PUMLGraph, PUMLNode
+    PUMLGraph
 )
 from tel2puml.puml_graph.graph_loop_extract import (
     extract_loops_starts_and_ends_from_loop
 )
+from tel2puml.tel2puml_types import PUMLEvent
 
 
 def insert_loops(puml_graph: PUMLGraph, loops: list[Loop]) -> None:
@@ -23,24 +24,10 @@ def insert_loop(puml_graph: PUMLGraph, loop: Loop):
         loop
     )
     for start_node, end_node in loop_starts_and_ends:
-        replace_loop_nodes_with_sub_graph_and_insert_sub_loops(
-            puml_graph, start_node, end_node, loop
+        sub_graph_node = (
+            puml_graph.replace_subgraph_node_from_start_and_end_nodes(
+                start_node, end_node, "LOOP",
+                PUMLEvent.LOOP
+            )
         )
-
-
-def replace_loop_nodes_with_sub_graph_and_insert_sub_loops(
-    puml_graph: PUMLGraph, start_node: PUMLNode, end_node: PUMLNode, loop: Loop
-) -> None:
-    """Replaces the loop nodes with a sub graph and inserts the sub loops.
-
-    :param start_node: The start node of the loop.
-    :type start_node: :class:`PUMLNode`
-    :param end_node: The end node of the loop.
-    :type end_node: :class:`PUMLNode`
-    :param loop: The loop to be inserted.
-    :type loop: :class:`Loop`
-    """
-    sub_graph = puml_graph.replace_subgraph_node_from_start_and_end_nodes(
-        start_node, end_node, "LOOP"
-    )
-    insert_loops(sub_graph, loop.sub_loops)
+        insert_loops(sub_graph_node.sub_graph, loop.sub_loops)
