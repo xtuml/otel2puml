d1f36daa0ed08abbf3d3edac3b33b64682ceb6fa
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-04-11 10:43:34 +0000

    CDSTH-687 Updated data creation to create test data correctly for pumls with branch counts present

--- tel2puml/pipelines/data_creation.py
+++ tel2puml/pipelines/data_creation.py
@@ -14,6 +14,7 @@ def generate_test_data(
     input_puml_file: str,
     template_all_paths: bool = True,
     num_paths_to_template: int = 1,
+    is_branch_puml: bool = False,
 ) -> Generator[dict, Any, None]:
     """
     This function creates test data from a puml file. It uses the
@@ -22,16 +23,21 @@ def generate_test_data(
     :type input_puml_file: `str`
     :param template_all_paths: If True, the function will template all paths
     in the sequence diagram. If False, the function will template a random
-    path in the sequence diagram.
-    :type template_all_paths: `bool`
+    path in the sequence diagram, defaults to `True`.
+    :type template_all_paths: `bool`, optional
     :param num_paths_to_template: The number of paths to template. This
-    parameter is only used if template_all_paths is False.
-    :type num_paths_to_template: `int`
+    parameter is only used if template_all_paths is False, defaults to `1`.
+    :type num_paths_to_template: `int`, optional
+    :param is_branch_puml: If True, the function will generate test data for
+    a puml file with branches, that is it will generate branch counts with two
+    and three branches, defaults to `False`.
+    :type is_branch_puml: `bool`, optional
     :return: A generator that yields the test data.
     :rtype: `Generator[dict, Any, None]`
     """
     for event_sequence in generate_test_data_event_sequences_from_puml(
-        input_puml_file, template_all_paths, num_paths_to_template
+        input_puml_file, template_all_paths, num_paths_to_template,
+        is_branch_puml
     ):
         yield from event_sequence
 
@@ -40,6 +46,7 @@ def generate_test_data_event_sequences_from_puml(
     input_puml_file: str,
     template_all_paths: bool = True,
     num_paths_to_template: int = 1,
+    is_branch_puml: bool = False,
 ) -> Generator[Generator[dict, Any, None], Any, None]:
     """This function creates test data from a puml file as distinct sequences.
     It uses the test_event_generator package to create the test data.
@@ -48,17 +55,30 @@ def generate_test_data_event_sequences_from_puml(
     :type input_puml_file: `str`
     :param template_all_paths: If True, the function will template all paths
     in the sequence diagram. If False, the function will template a random
-    path in the sequence diagram.
-    :type template_all_paths: `bool`
+    path in the sequence diagram, defaults to `True`.
+    :type template_all_paths: `bool`, optional
     :param num_paths_to_template: The number of paths to template. This
-    parameter is only used if template_all_paths is False.
-    :type num_paths_to_template: `int`
+    parameter is only used if template_all_paths is False, defaults to `1`.
+    :type num_paths_to_template: `int`, optional
+    :param is_branch_puml: If True, the function will generate test data for
+    a puml file with branches, that is it will generate branch counts with two
+    and three branches, defaults to `False`.
+    :type is_branch_puml: `bool`, optional
     :return: A generator that yields the test data.
     :rtype: `Generator[str, Any, None]`
     """
     test_job_templates = [
-        job for job in generate_valid_jobs_from_puml_file(input_puml_file)
+        job for job in generate_valid_jobs_from_puml_file(
+            input_puml_file,
+        )
     ]
+    if is_branch_puml:
+        test_job_templates.extend(
+            [job for job in generate_valid_jobs_from_puml_file(
+                input_puml_file,
+                num_branches=3
+            )]
+        )
     counter = 0
     if template_all_paths:
         for job in test_job_templates:
@@ -85,15 +105,20 @@ def generate_event_jsons(jobs: list[Job]) -> Generator[dict, Any, None]:
 
 def generate_valid_jobs_from_puml_file(
     input_puml_file: str,
+    **extra_options,
 ) -> Generator[Job, Any, None]:
     """This function generates valid jobs from a puml file.
 
     :param input_puml_file: The puml file that contains the sequence diagram.
     :type input_puml_file: `str`
+    :param extra_options: Extra options to pass to the test
+    event generator.
+    :type extra_options: `dict`
     :return: A generator that yields the valid jobs.
     :rtype: `Generator`[:class:`Job`, `Any`, `None`]
     """
     event_gen_options_to_use = {"invalid": False}
+    event_gen_options_to_use.update(extra_options)
     test_jobs_with_info = puml_file_to_test_events(
         input_puml_file, **event_gen_options_to_use
     )
