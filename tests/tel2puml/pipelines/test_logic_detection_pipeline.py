1c1a431b6837965ed5a787313f0d3345cdae697c
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-04-11 12:00:28 +0000

    CDSTH-687 Added tests for removing detected loops from logic detection

--- tests/tel2puml/pipelines/test_logic_detection_pipeline.py
+++ tests/tel2puml/pipelines/test_logic_detection_pipeline.py
@@ -11,8 +11,11 @@ from tel2puml.pipelines.logic_detection_pipeline import (
     Operator,
     update_all_connections_from_data,
     remove_detected_loop_events,
+    get_loop_events_to_remove_mapping,
+    remove_detected_loop_data_from_events,
 )
 from tel2puml.pipelines.data_creation import generate_test_data
+from tel2puml.detect_loops import Loop
 
 
 class TestOperator:
@@ -1299,3 +1302,65 @@ def test_get_logic_from_and_under_or_puml_file() -> None:
     for event in ["C", "D", "E", "F", "G"]:
         assert events_forward_logic[event].logic_gate_tree.label == "H"
         assert events_backward_logic[event].logic_gate_tree.label == "A"
+
+
+class TestLoopEdgeRemoval:
+    def get_loop_ref_and_events(
+        self
+    ) -> tuple[list[Loop], dict[str, str], dict[str, Event]]:
+        """Helper method to get loop reference and events for testing"""
+        # setup loops
+        loop_1 = Loop(["q1", "q2", "q3"])
+        loop_1.add_edge_to_remove(("q3", "q1"))
+        loop_2 = Loop(["q4", "q5"])
+        loop_2.add_edge_to_remove(("q5", "q5"))
+        loops = [loop_1, loop_2]
+        # setup node events reference
+        node_event_name_reference = {
+            "q1": "A",
+            "q2": "B",
+            "q3": "C",
+            "q4": "D",
+            "q5": "E",
+        }
+        # setup events
+        A = Event("A")
+        B = Event("B")
+        C = Event("C")
+        D = Event("D")
+        E = Event("E")
+        A.update_event_sets(["B"])
+        B.update_event_sets(["C"])
+        C.update_event_sets(["A"])
+        C.update_event_sets(["D"])
+        D.update_event_sets(["E"])
+        E.update_event_sets(["E"])
+        events = {"A": A, "B": B, "C": C, "D": D, "E": E}
+        return loops, node_event_name_reference, events
+
+    def test_get_loop_events_to_remove_mapping(self) -> None:
+        """Test for method get_loop_events_to_remove_mapping"""
+        loops, node_event_name_reference, _ = self.get_loop_ref_and_events()
+        loop_events_to_remove = get_loop_events_to_remove_mapping(
+            loops, node_event_name_reference
+        )
+        assert len(loop_events_to_remove) == 2
+        assert "C" in loop_events_to_remove and "E" in loop_events_to_remove
+        assert loop_events_to_remove["C"] == ["A"]
+        assert loop_events_to_remove["E"] == ["E"]
+
+    def test_remove_detected_loop_data_from_events(self) -> None:
+        """Test for method remove_detected_loop_data_from_events"""
+        loops, node_event_name_reference, events = (
+            self.get_loop_ref_and_events()
+        )
+        remove_detected_loop_data_from_events(
+            loops,
+            events,
+            node_event_name_reference
+        )
+        assert len(events["C"].event_sets) == 1
+        assert len(events["E"].event_sets) == 0
+        assert len(events["A"].event_sets) == 1
+        assert len(events["B"].event_sets) == 1
+        assert len(events["D"].event_sets) == 1
