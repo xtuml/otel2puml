7d57e12947fcbe9b79204f2036671672216ac1e2
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-04-25 08:50:57 +0000

    CDSTP-25 Tests for branch counts

---
+++ tests/end-to-end-tests/test_branch_counts.py
@@ -0,0 +1,17 @@
+"""End to end tests for branch counts.
+"""
+from tel2puml.utils_test import end_to_end_test
+
+
+def test_simple_branch_count() -> None:
+    end_to_end_test(
+        "end-to-end-pumls/branch_counts/simple_branch_count.puml",
+        is_branch_puml=True
+    )
+
+
+def test_double_branch_count() -> None:
+    end_to_end_test(
+        "end-to-end-pumls/branch_counts/double_branch_count.puml",
+        is_branch_puml=True
+    )
