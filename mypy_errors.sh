1ccc5750ae6814c8df137bc9e4f4d8c3a4150302
Ravinda Gunaratne <ravinda.gunaratne@smartdcs.co.uk>
2024-04-29 15:57:37 +0000

    CDSTP-34 Run other checks after mypy error file updates, jAlergia fixes

--- mypy_errors.sh
+++ mypy_errors.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 mypy --strict --explicit-package-bases --namespace-packages . > mypy_errors.txt
+
+# run checks immediately after updating mypy file
+flake8 .
+pylint --recursive=y .
+pytest
