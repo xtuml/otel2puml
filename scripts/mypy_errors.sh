76a8a738baaff8a42b297253b3a61b7bdc0bc3c0
Ravinda Gunaratne <ravinda.gunaratne@smartdcs.co.uk>
2024-04-30 15:56:25 +0000

    CDSTP-34 move to scripts

--- scripts/mypy_errors.sh
+++ scripts/mypy_errors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(git rev-parse --show-toplevel)
+
 mypy --config-file .mypy.test.ini --strict --explicit-package-bases --namespace-packages . > mypy_errors.txt
 
 # run checks immediately after updating mypy file
