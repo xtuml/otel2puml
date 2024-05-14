41f0e4620a8962a2401e01143ce65a2038b79afe
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-05-14 13:37:14 +0000

    CDSTP-31 Updated workflow and install repositories scripts to refer to most up to date versions of test harness and test event generator

--- scripts/install_repositories.sh
+++ scripts/install_repositories.sh
@@ -8,7 +8,7 @@ cd /tmp/
 git clone git@github.com:SmartDCSITlimited/test-event-generator.git
 cd test-event-generator
 git fetch --all --tags
-git checkout tags/v1.0.0 -b latest 
+git checkout tags/v1.0.1 -b latest 
 pip install -r requirements.txt
 pip install .
 cd ..
@@ -19,7 +19,7 @@ cd /tmp/
 git clone git@github.com:SmartDCSITlimited/test-harness.git
 cd test-harness
 git fetch --all --tags
-git checkout tags/v1.0.0 -b latest 
+git checkout tags/v1.1.3 -b latest 
 pip install -r requirements.txt
 pip install .
 cd ..
