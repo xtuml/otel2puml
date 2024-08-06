cde3754c784bcf61156d981d6860aad37c7d7593
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-08-06 15:07:53 +0000

    Add NodeModel class and association table

--- tel2puml/find_unique_graphs/otel_ingestion/otel_data_model.py
+++ tel2puml/find_unique_graphs/otel_ingestion/otel_data_model.py
@@ -2,6 +2,17 @@
 
 from typing import NamedTuple, Optional
 
+from sqlalchemy import (
+    Column,
+    String,
+    ForeignKey,
+    DateTime,
+    func,
+    Table,
+    Integer,
+)
+from sqlalchemy.orm import relationship, DeclarativeBase
+
 
 class OTelEvent(NamedTuple):
     """Named tuple for OTel event.
@@ -35,3 +46,59 @@ class OTelEvent(NamedTuple):
     application_name: str
     parent_event_id: str
     child_event_ids: Optional[list[str]] = None
+
+
+class Base(DeclarativeBase):
+    """Base class for SQLAlchemy models"""
+    pass
+
+
+node_association = Table(
+    "node_association",
+    Base.metadata,
+    Column("parent_id", Integer, ForeignKey("nodes.id"), primary_key=True),
+    Column("child_id", Integer, ForeignKey("nodes.id"), primary_key=True),
+)
+
+
+class NodeModel(Base):
+    """SQLAlchemy model representing a node in the graph structure."""
+
+    __tablename__ = "nodes"
+
+    id = Column(Integer, primary_key=True)
+    job_name = Column(String, nullable=False)
+    job_id = Column(String, nullable=False)
+    event_type = Column(String, nullable=False)
+    event_id = Column(String, unique=True, nullable=False)
+    start_timestamp = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    end_timestamp = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    application_name = Column(String, nullable=False)
+    parent_event_id = Column(String, ForeignKey("nodes.event_id"))
+
+    children = relationship(
+        "NodeModel",
+        secondary=node_association,
+        primaryjoin=(id == node_association.c.parent_id),
+        secondaryjoin=(id == node_association.c.child_id),
+        backref="parents",
+    )
+
+    def __repr__(self) -> str:
+        return f"""
+        <NodeModel(
+        job_name='{self.job_name}',
+        job_id='{self.job_id}',
+        event_type='{self.event_type}',
+        event_id='{self.event_id}',
+        start_timestamp='{self.start_timestamp}',
+        end_timestamp='{self.end_timestamp}',
+        application_name='{self.application_name},
+        parent_event_id='{self.parent_event_id}'
+        child_event_ids='{[child.event_id for child in self.children]}'
+        )>
+        """
