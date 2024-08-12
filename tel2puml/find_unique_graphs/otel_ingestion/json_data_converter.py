ae8268b79a3cd98e51bec163a12194e35e82bdc4
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-08-12 09:31:04 +0000

    Add caching of previously visited results

--- tel2puml/find_unique_graphs/otel_ingestion/json_data_converter.py
+++ tel2puml/find_unique_graphs/otel_ingestion/json_data_converter.py
@@ -7,6 +7,8 @@ from tel2puml.find_unique_graphs.otel_ingestion.otel_data_model import (
     JSONDataSourceConfig,
 )
 
+MAX_SEGMENT_COUNT = 50  # TODO have a think about this
+
 
 def flatten_json_dict(json_data: dict[str, Any]) -> Any:
     """Function to flatten a nested dictionary.
@@ -35,8 +37,12 @@ def map_data_to_json_schema(
     """
     result: dict[str, str] = {}
     field_mapping: dict[str, Any] = mapping_config["field_mapping"]
+    # Cache results within lists to avoid looping over them again
+    field_cache: dict[str, str] = {}
     for field_name, field_config in field_mapping.items():
-        process_field(field_name, field_config, flattened_data, result)
+        process_field(
+            field_name, field_config, flattened_data, result, field_cache
+        )
     return result
 
 
@@ -45,6 +51,7 @@ def process_field(
     field_config: dict[str, Any],
     flattened_data: Any,
     result: dict[str, str],
+    field_cache: dict[str, str],
 ) -> None:
     """
     Function that processes a single field according to its configuration.
@@ -57,13 +64,23 @@ def process_field(
     :type flattened_data: `Any`
     :param result: The mapped data
     :type result: `dict`[`str`, `str`]
+    :param field_cache: Cache for optimising field access and path generation
+    in flattened JSON data
+    :type field_cache: `dict`[`str`, [`str`]]
     """
     for index, key_path in enumerate(field_config["key_paths"]):
         path_segments = key_path.split(":")
         if "" in path_segments:
+            field_cache_key = key_path.split("::")[0]
             # Indicates that we are handling a list within the json data
             handle_empty_segments(
-                field_name, field_config, index, flattened_data, result
+                field_name,
+                field_config,
+                index,
+                flattened_data,
+                result,
+                field_cache,
+                field_cache_key,
             )
         else:
             full_path = ":".join(path_segments)
@@ -83,6 +100,8 @@ def handle_empty_segments(
     index: int,
     flattened_data: Any,
     result: dict[str, str],
+    field_cache: dict[str, str],
+    field_cache_key: str,
 ) -> None:
     """
     Function that handles paths with empty segments, indicating a list within
@@ -98,41 +117,217 @@ def handle_empty_segments(
     :type flattened_data: `Any`
     :param result: The mapped data
     :type result: `dict`[`str`, `str`]
+    :param field_cache: Cache for optimising field access and path generation
+    in flattened JSON data
+    :type field_cache: `dict`[`str`, [`str`]]
+    :param field_cache_key: The key of the field cache
+    :type field_cache_key: `str`
     """
     full_path = ":".join(field_config["key_paths"][index].split(":"))
-    segment_count = 0
     key = full_path.split("::")[-1]
-    full_path = full_path.replace("::", f":{str(segment_count)}:")
 
-    # Avoid an infinite loop
     # TODO Have a think about this number
-    while segment_count <= 50:
+    if field_name == "event_type":
+        pass
+    for segment_count in range(MAX_SEGMENT_COUNT):
         try:
-            if flattened_data[full_path] == field_config["key_value"][index]:
-                full_path = full_path.replace(
+            full_path = update_full_path(full_path, segment_count)
+
+            cache_entry = get_or_create_cache_entry(
+                field_cache, field_cache_key, key
+            )
+
+            # check for case where we have attributes::cloudProvider
+            if not field_config.get("key_value") and flattened_data.get(
+                full_path
+            ):
+                result[field_name] = flattened_data.get(full_path)
+                return
+
+            value_to_check = field_config["key_value"][index]
+            full_path = get_cached_path(
+                cache_entry, value_to_check, full_path, segment_count
+            )
+
+            if is_matching_data(flattened_data, full_path, value_to_check):
+                update_cache(cache_entry, flattened_data, full_path)
+                process_matching_data(
+                    field_name,
+                    field_config,
+                    index,
+                    flattened_data,
+                    full_path,
                     key,
-                    field_config["value_paths"][index],
+                    result,
                 )
-                value_type = get_value_type(field_config)
-                add_or_append_value(
-                    field_name, flattened_data[full_path], result, value_type
-                )
-                break
+                return
+            update_cache(cache_entry, flattened_data, full_path)
+
         except KeyError:
-            # Key errors will occur as we are looping through a list trying to
-            # find the key iteratively
-            pass
+            cache_entry["count"] += 1
         except Exception as e:
             raise RuntimeError(f"An error occured - {e}")
 
-        full_path = full_path.replace(
-            f":{str(segment_count)}:",
-            f":{str(segment_count + 1)}:",
-        )
-        segment_count += 1
+    raise RuntimeError(
+        f"Could not find data for '{field_name}' within {MAX_SEGMENT_COUNT}"
+        " segments"
+    )
 
-    if segment_count > 50:
-        raise RuntimeError(f"Could not find data for '{field_name}'")
+
+def update_full_path(full_path: str, segment_count: int) -> str:
+    """
+    Function that updates the full path by replacing segment count placeholders
+    with actual segment counts.
+
+    :param full_path: The original path containing placeholders
+    :type full_path: `str`
+    :param segment_count: The current segment count to be inserted into
+    the path
+    :type segment_count: `int`
+    :return: The updated path with the correct segment count
+    :rtype: `str`
+    """
+    if segment_count == 0:
+        return full_path.replace("::", f":{str(segment_count)}:")
+    return full_path.replace(f":{segment_count -1}:", f":{segment_count}:")
+
+
+def get_or_create_cache_entry(
+    field_cache: dict[str, Any], field_cache_key: str, key: str
+) -> dict[str, Any]:
+    """
+    Function that retrieves an existing cache entry or creates a new one if it
+    doesn't exist.
+
+    :param field_cache: The cache dictionary containing field entries
+    :type field_cache: `dict`[`str`, `Any`]
+    :param field_cache_key: The key to look up or create in the field cache
+    :type field_cache_key: `str`
+    :return: The existing or newly created cache entry for the given key
+    :rtype: `dict`[`str`,`Any`]
+    """
+    if field_cache_key not in field_cache:
+        field_cache[field_cache_key] = {key: {"count": 0}}
+    else:
+        if key not in field_cache[field_cache_key]:
+            field_cache[field_cache_key] = {key: {"count": 0}}
+    cache_entry = field_cache[field_cache_key][key]
+    if isinstance(cache_entry, dict):
+        return cache_entry
+    else:
+        raise TypeError("Field cache entry should be of type dict.")
+
+
+def get_cached_path(
+    cache_entry: dict[str, Any],
+    value_to_check: str,
+    full_path: str,
+    segment_count: int,
+) -> str:
+    """
+    Function that retrieves a cached path or generates a new one based on the
+    provided parameters.
+
+    :param cache_entry: The cache entry containing paths and a count
+    :type cache_entry: `dict`[`str`, `Any`]
+    :param value_to_check: The value to look up in the cache entry
+    :type value_to_check: `Any`
+    :param full_path: The original full path to modify if the value is not
+    found in the cache
+    :type full_path: `str`
+    :param segment_count: The current segment count in the full path to
+    be replaced
+    :type segment_count: `int`
+    :return: The cached path if found, otherwise a new path with updated
+    segment count
+    :rtype: `str`
+    """
+
+    cached_path = cache_entry.get(
+        value_to_check,
+        full_path.replace(f":{segment_count}:", f":{cache_entry['count']}:"),
+    )
+    if isinstance(cached_path, str):
+        return cached_path
+    else:
+        raise TypeError("Path should be of type string.")
+
+
+def is_matching_data(
+    flattened_data: dict[str, str], full_path: str, value_to_check: str
+) -> bool:
+    """
+    Function that checks if the data at the given path in flattened_data
+    matches the value_to_check.
+
+    :param flattened_data: The flattened JSON data to check
+    :type flattened_data: `dict`[`str`, `str`]
+    :param full_path: The path to check in the flattened data
+    :type full_path: `str`
+    :param value_to_check: The value to compare against the data at full_path
+    :type value_to_check: `str`
+    :return: True if the data matches, False otherwise
+    :rtype: `bool`
+    """
+    return (
+        full_path in flattened_data
+        and flattened_data[full_path] == value_to_check
+    )
+
+
+def process_matching_data(
+    field_name: str,
+    field_config: dict[str, Any],
+    index: int,
+    flattened_data: dict[str, Any],
+    full_path: str,
+    key: str,
+    result: dict[str, str],
+) -> None:
+    """Function to process data that matches the search requirements. The
+    result is either added as a new value or appended to an existing one.
+
+    :param field_name: The field name within the mapping config
+    :type field_name: `str`
+    :param field_config: The config for the field name
+    :type field_config: `dict`[`str`,`Any`]
+    :param index: The index of the field config
+    :type index: `int`
+    :param flattened_data: JSON data as a flattened dictionary
+    :type flattened_data: `Any`
+    :param full_path: The original path containing placeholders
+    :type full_path: `str`
+    :param key: The key within the full path to replace
+    :type key: `str`
+    :param result: The mapped data
+    :type result: `dict`[`str`, `str`]
+    """
+    value_path = full_path.replace(key, field_config["value_paths"][index])
+    value_type = get_value_type(field_config)
+    add_or_append_value(
+        field_name, flattened_data[value_path], result, value_type
+    )
+
+
+def update_cache(
+    cache_entry: dict[str, Any],
+    flattened_data: dict[str, str],
+    full_path: str,
+) -> None:
+    """Function to update the cache.
+
+    :param cache_entry: The current entry within the cache
+    :type cache_entry: `dict`[str, `Any`]
+    :param flattened_data: JSON data as a flattened dictionary
+    :type flattened_data: `Any`
+    :param full_path: The path to update within the cache
+    :type full_path: `str`
+    """
+    try:
+        cache_entry[flattened_data[full_path]] = full_path
+    except TypeError:
+        pass
+    cache_entry["count"] += 1
 
 
 def handle_regular_path(
