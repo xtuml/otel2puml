b649a716ca90840b02a40ce50f08e3d01ba98c0d
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-08-13 13:37:52 +0000

    Update tests

--- tests/tel2puml/find_unique_graphs/test_otel_data_source.py
+++ tests/tel2puml/find_unique_graphs/test_otel_data_source.py
@@ -1,6 +1,7 @@
 """Tests for otel_data_source module."""
 
 import json
+import yaml
 import pytest
 from typing import Any
 
@@ -9,84 +10,64 @@ from unittest.mock import mock_open, patch
 from tel2puml.find_unique_graphs.otel_ingestion.otel_data_source import (
     JSONDataSource,
 )
-from tel2puml.find_unique_graphs.otel_ingestion.otel_data_model import (
-    OTelEvent,
-)
-
-
-class TestOTELDataSource:
-    """Tests for OTelDataSource class"""
-
-    @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_get_yaml_config() -> None:
-        """Tests parsing yaml config file"""
-        data_source = JSONDataSource()
-        assert isinstance(data_source.yaml_config, dict)
-        assert data_source.yaml_config["ingest_data"]["data_source"] == "json"
-
-    @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_set_data_source_valid() -> None:
-        """Tests for valid file ext."""
-        json_data_source = JSONDataSource()
-        assert json_data_source.data_source == "json"
-
-    @staticmethod
-    def test_set_data_source_invalid(mock_yaml_config_string: str) -> None:
-        """Tests invalid file extension in yaml config."""
-        invalid_yaml = mock_yaml_config_string.replace("json", "invalid")
-        with patch("builtins.open", mock_open(read_data=invalid_yaml)), patch(
-            "os.path.isdir", return_value=True
-        ), patch("os.path.isfile", return_value=True):
-            with pytest.raises(ValueError, match="is not a valid data source"):
-                JSONDataSource()
 
 
 class TestJSONDataSource:
     """Tests for JSONDataSource class."""
 
     @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_set_dirpath_valid() -> None:
+    @pytest.mark.usefixtures("mock_path_exists")
+    def test_get_yaml_config(mock_yaml_config_dict: dict[str, Any]) -> None:
+        """Tests parsing yaml config file"""
+        json_data_source = JSONDataSource(mock_yaml_config_dict)
+        assert isinstance(json_data_source.config, dict)
+        assert json_data_source.config["data_location"] == "resource_spans"
+        assert isinstance(
+            json_data_source.config["header_mapping"]["header"]["key_paths"],
+            list,
+        )
+        assert (
+            json_data_source.config["header_mapping"]["header"]["key_paths"][0]
+            == "resource:attributes::key"
+        )
+
+    @staticmethod
+    @pytest.mark.usefixtures("mock_path_exists")
+    def test_set_dirpath_valid(mock_yaml_config_dict: dict[str, Any]) -> None:
         """Tests set_dirpath method."""
-        json_data_source = JSONDataSource()
+        json_data_source = JSONDataSource(mock_yaml_config_dict)
         assert json_data_source.dirpath == "/path/to/json/directory"
 
     @staticmethod
-    @pytest.mark.usefixtures("mock_yaml_config")
-    def test_set_dirpath_invalid() -> None:
+    def test_set_dirpath_invalid(
+        mock_yaml_config_dict: dict[str, Any],
+    ) -> None:
         """Tests the set_dirpath method with a non-existant directory."""
         with patch("os.path.isdir", return_value=False), patch(
             "os.path.isfile", return_value=True
         ):
             with pytest.raises(ValueError, match="directory does not exist"):
-                JSONDataSource()
+                JSONDataSource(mock_yaml_config_dict)
 
     @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_set_filepath_valid() -> None:
+    @pytest.mark.usefixtures("mock_path_exists")
+    def test_set_filepath_valid(
+        mock_yaml_config_dict: dict[str, Any],
+    ) -> None:
         """Tests the set_filepath method."""
-        jsond_data_source = JSONDataSource()
+        jsond_data_source = JSONDataSource(mock_yaml_config_dict)
         assert jsond_data_source.filepath == "/path/to/json/file.json"
 
     @staticmethod
-    @pytest.mark.usefixtures("mock_yaml_config")
-    def test_set_filepath_invalid() -> None:
+    def test_set_filepath_invalid(
+        mock_yaml_config_dict: dict[str, Any],
+    ) -> None:
         """Tests the set_filepath method with an non-existant file."""
         with patch("os.path.isdir", return_value=True), patch(
             "os.path.isfile", return_value=False
         ):
             with pytest.raises(ValueError, match="does not exist"):
-                JSONDataSource()
+                JSONDataSource(mock_yaml_config_dict)
 
     @staticmethod
     def test_initialisation_no_dirpath_no_filepath(
@@ -97,120 +78,79 @@ class TestJSONDataSource:
             "dirpath: /path/to/json/directory", 'dirpath: ""'
         ).replace("filepath: /path/to/json/file.json", 'filepath: ""')
 
-        with patch("builtins.open", mock_open(read_data=invalid_yaml)), patch(
-            "os.path.isdir", return_value=False
-        ), patch("os.path.isfile", return_value=False):
-            with pytest.raises(
-                FileNotFoundError, match="No directory or files found"
-            ):
-                JSONDataSource()
+        config = yaml.safe_load(invalid_yaml)
+
+        with pytest.raises(
+            FileNotFoundError, match="No directory or files found"
+        ):
+            JSONDataSource(config)
 
     @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_init() -> None:
+    @pytest.mark.usefixtures("mock_path_exists")
+    def test_init(
+        mock_yaml_config_dict: dict[str, Any],
+    ) -> None:
         """Tests the classes init method."""
-        json_data_source = JSONDataSource()
-        assert json_data_source.data_source == "json"
+        json_data_source = JSONDataSource(mock_yaml_config_dict)
         assert json_data_source.dirpath == "/path/to/json/directory"
         assert json_data_source.filepath == "/path/to/json/file.json"
 
     @staticmethod
     @pytest.mark.usefixtures("mock_path_exists")
     def test_get_file_list_filepath(
-        mock_yaml_config_string: str,
+        mock_yaml_config_dict: dict[str, Any],
     ) -> None:
         """Tests getting the json filepath if directory is not specified."""
-        no_dir_path_yaml = mock_yaml_config_string.replace(
-            "dirpath: /path/to/json/directory", 'dirpath: ""'
-        )
-        with patch("builtins.open", mock_open(read_data=no_dir_path_yaml)):
-            json_data_source = JSONDataSource()
-            assert json_data_source.get_file_list() == [
-                "/path/to/json/file.json"
-            ]
-
-    @staticmethod
-    @pytest.mark.usefixtures("mock_yaml_config", "mock_path_exists")
-    def test_get_file_list_dirpath(
-        mock_file_list: list[str],
-    ) -> None:
-        """Tests getting the json filepaths if the directory is specified."""
-        with patch(
-            "os.listdir",
-            return_value=["/mock/dir/file1.json", "/mock/dir/file2.json"],
-        ):
-            json_data_source = JSONDataSource()
-            assert json_data_source.get_file_list() == mock_file_list
-
-    @staticmethod
-    @pytest.mark.usefixtures("mock_path_exists")
-    def test_parse_json_stream(
-        mock_json_data: list[dict[str, Any]],
-        mock_yaml_config_string: str,
-    ) -> None:
-        """Tests parsing a json file."""
-        no_dir_path_yaml = mock_yaml_config_string.replace(
-            "dirpath: /path/to/json/directory", 'dirpath: ""'
-        )
-        with patch("builtins.open", mock_open(read_data=no_dir_path_yaml)):
-            json_data_source = JSONDataSource()
-            mock_file_content = json.dumps(mock_json_data)
-
-            with patch(
-                "builtins.open", mock_open(read_data=mock_file_content)
-            ):
-                events = list(
-                    json_data_source.parse_json_stream("mock_file.json")
-                )
-
-            assert len(events) == 2
-            assert isinstance(events[0], OTelEvent)
-            assert events[0].event_id == "event1"
-            assert events[1].event_id == "event2"
-
-    @staticmethod
-    @pytest.mark.usefixtures(
-        "mock_yaml_config", "mock_path_exists", "mock_filepath_in_dir"
-    )
-    def test_create_otel_object(
-        mock_json_data: list[dict[str, Any]],
-    ) -> None:
-        """Tests creating an OTelEvent object."""
-        json_data_source = JSONDataSource()
-        otel_event = json_data_source.create_otel_object(mock_json_data[0])
-
-        assert isinstance(otel_event, OTelEvent)
-        assert otel_event.job_name == "job1"
-        assert otel_event.job_id == "id1"
-        assert otel_event.event_type == "type1"
-        assert otel_event.event_id == "event1"
-        assert otel_event.start_timestamp == "2023-01-01T00:00:00"
-        assert otel_event.end_timestamp == "2023-01-01T00:01:00"
-        assert otel_event.application_name == "app1"
-        assert otel_event.parent_event_id is None
-        assert otel_event.child_event_ids == ["event2"]
+        json_data_source = JSONDataSource(mock_yaml_config_dict)
+        json_data_source.dirpath = ""
+        assert json_data_source.get_file_list() == ["/path/to/json/file.json"]
 
     @staticmethod
     @pytest.mark.usefixtures("mock_path_exists", "mock_filepath_in_dir")
-    def test_iter_next(
-        mock_yaml_config_string: str,
+    def test_parse_json_stream(
         mock_json_data: list[dict[str, Any]],
+        mock_yaml_config_dict: dict[str, Any],
     ) -> None:
-        """Tests the __iter__ method."""
+        """Tests parsing a json file."""
+        mock_file_content = json.dumps(mock_json_data)
+
         with patch(
-            "builtins.open", mock_open(read_data=mock_yaml_config_string)
+            "builtins.open", mock_open(read_data=mock_file_content)
+        ), patch.object(
+            JSONDataSource,
+            "get_file_list",
+            return_value=["/mock/dir/file1.json"],
         ):
-            json_data_source = JSONDataSource()
-            mock_file_content = json.dumps(mock_json_data)
+            json_data_source = JSONDataSource(mock_yaml_config_dict)
+            json_data_source.config["dirpath"] = ""
 
-            with patch(
-                "builtins.open", mock_open(read_data=mock_file_content)
-            ):
-                events = list(json_data_source)
+            grouped_spans = dict()
+            for data, header in json_data_source:
+                grouped_spans.setdefault(header, [])
+                grouped_spans[header].append(data)
 
-        assert len(events) == 2
-        assert isinstance(events[0], OTelEvent)
-        assert events[0].event_id == "event1"
-        assert events[1].event_id == "event2"
+        assert len(grouped_spans) == 2
+        assert grouped_spans["Frontend 1.0"]
+        assert grouped_spans["Backend 1.2"]
+
+        otel_event = grouped_spans["Frontend 1.0"][0]
+        assert otel_event.job_name == "Azure"
+        assert otel_event.job_id == "B4MQWcR6iByyOq4EMSs5Nn== 4.8"
+        assert otel_event.event_id == "F1Vp3ypcQfU=="
+        assert otel_event.event_type == "com.T2h.366Yx 500"
+        assert otel_event.application_name == "Processor 1.2"
+        assert otel_event.start_timestamp == "2024-08-13 10:15:32"
+        assert otel_event.end_timestamp == "2024-08-13 10:15:32"
+        assert otel_event.parent_event_id == "NzWDkmlAnji=="
+        assert otel_event.child_event_ids == None
+
+        otel_event2 = grouped_spans["Frontend 1.0"][1]
+        assert otel_event2.job_name == "AWS"
+        assert otel_event2.job_id == "Js7TGf4OJROjbISB1BvOOb== 2.0"
+        assert otel_event2.event_id == "Jv6moYFCoLK=="
+        assert otel_event2.event_type == "com.C36.9ETRp 401"
+        assert otel_event2.application_name == "Handler 2.9"
+        assert otel_event2.start_timestamp == "2024-08-13 10:15:32"
+        assert otel_event2.end_timestamp == "2024-08-13 10:15:32"
+        assert otel_event2.parent_event_id == "0u4wIXKIZ2t=="
+        assert otel_event2.child_event_ids == None
