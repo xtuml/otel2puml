25a3d99d828d96dcc50fb98f8a30fb4ffeb4c75f
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-08-22 17:14:40 +0000

    61 - Created create_temp_table_of_root_nodes_in_time_window

--- tel2puml/find_unique_graphs/find_unique_graphs.py
+++ tel2puml/find_unique_graphs/find_unique_graphs.py
@@ -1,8 +1,14 @@
 """Module for finding unique graphs in a list of graphs from ingested
 OpenTelemetry data."""
 
+import sqlalchemy as sa
+
+from tel2puml.find_unique_graphs.otel_ingestion.otel_data_model import (
+    NodeModel,
+)
 from tel2puml.find_unique_graphs.otel_ingestion.otel_data_holder import (
     DataHolder,
+    SQLDataHolder,
 )
 
 
@@ -35,3 +41,54 @@ def get_time_window(
         data_holder.min_timestamp + time_buffer_in_nanoseconds,
         data_holder.max_timestamp - time_buffer_in_nanoseconds,
     )
+
+
+def create_temp_table_of_root_nodes_in_time_window(
+    time_window: tuple[int, int], sql_data_holder: SQLDataHolder
+) -> sa.Table:
+    """Create a temporary table with the root nodes in the time window.
+
+    :param time_window: The time window for the unique graphs
+    :type time_window: `tuple[int, int]`
+    :param sql_data_holder: The SQL data holder object containing the ingested
+        data
+    :type sql_data_holder: :class:`SQLDataHolder`
+    :return: The temporary table with the root nodes in the time window
+    :rtype: :class:`sa.Table`
+    """
+    temp_table = sa.Table(
+        "temp_root_nodes",
+        sql_data_holder.base.metadata,
+        sa.Column("event_id", sa.String, unique=True, primary_key=True),
+        prefixes=["TEMPORARY"],
+    )
+
+    with sql_data_holder.session as session:
+        stmt = (
+            session.query(NodeModel.job_id)
+            .group_by(NodeModel.job_id)
+            .having(
+                sa.func.count(1).filter(
+                    (
+                        (NodeModel.start_timestamp <= time_window[1])
+                        & (NodeModel.start_timestamp >= time_window[0])
+                    )
+                    | (
+                        (NodeModel.end_timestamp <= time_window[1])
+                        & (NodeModel.end_timestamp >= time_window[0])
+                    )
+                )
+                > 0
+            )
+            .subquery()
+        )
+        stmt = (
+            sa.select(NodeModel.event_id)
+            .join(stmt, NodeModel.job_id == stmt.c.job_id)
+            .where(NodeModel.parent_event_id.is_(None))
+        )
+        session.execute(sa.schema.CreateTable(temp_table))
+        session.execute(temp_table.insert().from_select(["event_id"], stmt))
+        session.commit()
+
+    return temp_table
