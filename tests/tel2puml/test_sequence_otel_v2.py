5eb725f7c07b877f5a390eb6f9c6fcb35f8b8b9e
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-09-04 10:03:54 +0000

    55 - Create test for order_groups_by_start_timestamp

---
+++ tests/tel2puml/test_sequence_otel_v2.py
@@ -0,0 +1,70 @@
+"""Test the tel2puml.sequence_otel_v2 module."""
+
+import pytest
+
+from tel2puml.sequence_otel_v2 import order_groups_by_start_timestamp
+from tel2puml.find_unique_graphs.otel_ingestion.otel_data_model import (
+    OTelEvent,
+)
+
+
+def test_order_groups_by_start_timestamp() -> None:
+    """Test order_groups_by_start_timestamp."""
+    groups = [
+        [
+            OTelEvent(
+                job_name="job_name",
+                job_id="job_id",
+                event_type="event_type",
+                event_id="event_id",
+                start_timestamp=i * 2 + j,
+                end_timestamp=i * 2 + j + 1,
+                application_name="application_name",
+                parent_event_id=None,
+                child_event_ids=None,
+            )
+            for j in reversed(range(2))
+        ]
+        for i in reversed(range(3))
+    ]
+    ordered_groups = order_groups_by_start_timestamp(groups)
+    expected_ordered_groups = [
+        [
+            OTelEvent(
+                job_name="job_name",
+                job_id="job_id",
+                event_type="event_type",
+                event_id="event_id",
+                start_timestamp=i * 2 + j,
+                end_timestamp=i * 2 + j + 1,
+                application_name="application_name",
+                parent_event_id=None,
+                child_event_ids=None,
+            )
+            for j in range(2)
+        ]
+        for i in range(3)
+    ]
+    assert ordered_groups == expected_ordered_groups
+    # test case where one group is empty
+    with pytest.raises(ValueError):
+        order_groups_by_start_timestamp(
+            [
+                [],
+                [
+                    OTelEvent(
+                        job_name="job_name",
+                        job_id="job_id",
+                        event_type="event_type",
+                        event_id="event_id",
+                        start_timestamp=0,
+                        end_timestamp=1,
+                        application_name="application_name",
+                        parent_event_id=None,
+                        child_event_ids=None,
+                    )
+                ],
+            ]
+        )
+    # test case where there are no groups
+    assert order_groups_by_start_timestamp([]) == []
