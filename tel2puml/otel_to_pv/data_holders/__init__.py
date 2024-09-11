70e6272caf0c0d1ea0e558708a920c0cde9a9934
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-09-11 14:41:43 +0000

    100 - fix mypy and linting errors

--- tel2puml/otel_to_pv/data_holders/__init__.py
+++ tel2puml/otel_to_pv/data_holders/__init__.py
@@ -1,3 +1,5 @@
 """init file for data_holders package."""
-from .sql_data_holder.sql_dataholder import SQLDataHolder
-from .base import DataHolder
+from .sql_data_holder.sql_dataholder import SQLDataHolder as SQLDataHolder
+from .base import DataHolder as DataHolder
+
+__all__ = ["SQLDataHolder", "DataHolder"]
