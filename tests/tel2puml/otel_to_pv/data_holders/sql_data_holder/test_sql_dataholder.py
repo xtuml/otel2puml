0ceb9d963c211db7f51e7118125f8fdea72eaa6c
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-09-17 07:42:53 +0000

    Fix mypy error

--- tests/tel2puml/otel_to_pv/data_holders/sql_data_holder/test_sql_dataholder.py
+++ tests/tel2puml/otel_to_pv/data_holders/sql_data_holder/test_sql_dataholder.py
@@ -830,7 +830,7 @@ def test_stream_job_name_batches(
         all_events = list(otel_event_gen)
 
     assert len(all_events) == 4
-    job_id_count: dict[str, int] = {}
+    job_id_count = {}
     expected_event_ids = ["0_0", "0_1", "1_0", "1_1"]
     for otel_event in all_events:
         job_id_count.setdefault(otel_event.job_id, 0)
