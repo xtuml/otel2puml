ebf89049195d312000392bd5b3c865e023e09107
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-09-20 13:34:21 +0000

    Add otel_to_pv function and tests

--- tests/tel2puml/otel_to_pv/test_sequence_otel_v2.py
+++ tests/tel2puml/otel_to_pv/test_sequence_otel_v2.py
@@ -18,6 +18,7 @@ from tel2puml.otel_to_pv.sequence_otel_v2 import (
     sequence_otel_job_id_streams,
     job_ids_to_eventid_to_otelevent_map,
     config_to_otel_job_name_group_streams,
+    otel_to_pv,
 )
 from tel2puml.otel_to_pv.otel_to_pv_types import OTelEvent
 from tel2puml.tel2puml_types import PVEvent
@@ -600,5 +601,112 @@ class TestSeqeunceOTelJobs:
 
         assert len(events) == 8
         assert all(isinstance(event, OTelEvent) for event in events)
+        assert all(count == 2 for count in job_id_count.values())
+        assert len(valid_event_ids) == 0
+
+    def test_otel_to_pv(
+        self,
+        monkeypatch: MonkeyPatch,
+        mock_yaml_config_dict: dict[str, Any],
+        sql_data_holder_with_otel_jobs: SQLDataHolder,
+        mock_yaml_config_string: str,
+        mock_temp_dir_with_json_files: Path,
+    ) -> None:
+        """Tests for the function otel_to_pv."""
+
+        # Test 1: Default parameters
+        def mock_fetch_data_holder(config: IngestDataConfig) -> SQLDataHolder:
+            return sql_data_holder_with_otel_jobs
+
+        ingest_data_config = IngestDataConfig(
+            data_sources=mock_yaml_config_dict["data_sources"],
+            data_holders=mock_yaml_config_dict["data_holders"],
+            ingest_data=IngestTypes(**mock_yaml_config_dict["ingest_data"]),
+        )
+        monkeypatch.setattr(
+            "tel2puml.otel_to_pv.sequence_otel_v2.fetch_data_holder",
+            mock_fetch_data_holder,
+        )
+
+        result = otel_to_pv(ingest_data_config)
+
+        events = []
+        valid_job_names = {"test_name"}
+        valid_event_ids = [f"{i}_{j}" for i in range(5) for j in range(2)]
+        valid_job_ids = {f"test_id_{i}" for i in range(5)}
+        job_id_count: dict[str, int] = {}
+        for job_name, pv_event_streams in result:
+            assert job_name in valid_job_names
+            for pv_event_gen in pv_event_streams:
+                for pv_event in pv_event_gen:
+                    job_id_count.setdefault(pv_event["jobId"], 0)
+                    job_id_count[pv_event["jobId"]] += 1
+                    events.append(pv_event)
+                    assert pv_event["eventId"] in valid_event_ids
+                    assert pv_event["jobId"] in valid_job_ids
+                    valid_event_ids.remove(pv_event["eventId"])
+
+        assert len(events) == 10
+        assert all(count == 2 for count in job_id_count.values())
+        assert len(valid_event_ids) == 0
+
+        # Test 2: ingest_data = True
+        config = self.get_ingest_config(
+            mock_yaml_config_string, mock_temp_dir_with_json_files
+        )
+        result = otel_to_pv(config, ingest_data=True)
+
+        events = []
+        valid_job_names = {"Backend TestJob", "Frontend TestJob"}
+        valid_job_ids = {
+            "0_trace_id_1 4.8",
+            "1_trace_id_1 4.8",
+            "0_trace_id_0 4.8",
+            "1_trace_id_0 4.8",
+        }
+        valid_event_ids = [
+            f"{i}_span_{j}_{k}"
+            for i in range(2)
+            for j in range(2)
+            for k in range(2)
+        ]
+        job_id_count = {}
+        for job_name, pv_event_streams in result:
+            assert job_name in valid_job_names
+            for pv_event_gen in pv_event_streams:
+                for pv_event in pv_event_gen:
+                    job_id_count.setdefault(pv_event["jobId"], 0)
+                    job_id_count[pv_event["jobId"]] += 1
+                    events.append(pv_event)
+                    assert pv_event["eventId"] in valid_event_ids
+                    assert pv_event["jobId"] in valid_job_ids
+                    valid_event_ids.remove(pv_event["eventId"])
+
+        assert len(events) == 8
+        assert all(count == 2 for count in job_id_count.values())
+        assert len(valid_event_ids) == 0
+
+        # Test 3: find_unique_graphs = True
+        result = otel_to_pv(ingest_data_config, find_unique_graphs=True)
+
+        events = []
+        valid_job_names = {"test_name"}
+        # job id test_id_0 is outside the config time buffer window, therefore
+        # it is not included, reducing total events streamed to 8
+        valid_event_ids = [f"{i}_{j}" for i in range(1, 5) for j in range(2)]
+        valid_job_ids = {f"test_id_{i}" for i in range(1, 5)}
+        job_id_count = {}
+        for job_name, pv_event_streams in result:
+            assert job_name in valid_job_names
+            for pv_event_gen in pv_event_streams:
+                for pv_event in pv_event_gen:
+                    job_id_count.setdefault(pv_event["jobId"], 0)
+                    job_id_count[pv_event["jobId"]] += 1
+                    events.append(pv_event)
+                    assert pv_event["eventId"] in valid_event_ids
+                    assert pv_event["jobId"] in valid_job_ids
+                    valid_event_ids.remove(pv_event["eventId"])
+
+        assert len(events) == 8
         assert all(count == 2 for count in job_id_count.values())
         assert len(valid_event_ids) == 0
