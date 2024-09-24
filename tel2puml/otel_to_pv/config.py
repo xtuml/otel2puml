026b0cf923cab5d2328d5b51a43ccbfc8bf653d7
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-09-24 11:04:52 +0000

    113 - updated typing and added docstrings

--- tel2puml/otel_to_pv/config.py
+++ tel2puml/otel_to_pv/config.py
@@ -9,6 +9,7 @@ from typing import (
     Any,
     Iterable,
     Self,
+    Sequence
 )
 
 from pydantic import BaseModel, ConfigDict as PYDConfigDict
@@ -29,22 +30,35 @@ class SpanSpec(TypedDict):
 class FieldSpec(TypedDict):
     """Typed dict for FieldSpec."""
 
-    key_paths: list[str | tuple[str, ...]]
-    key_value: Optional[list[Optional[Union[str, tuple[str | None, ...]]]]]
-    value_paths: Optional[list[Optional[Union[str, tuple[str | None, ...]]]]]
+    key_paths: list[str | Iterable[str]]
+    key_value: Optional[list[Optional[Union[str, Iterable[str | None]]]]]
+    value_paths: Optional[list[Optional[Union[str, Iterable[str | None]]]]]
     value_type: Union[str, int]
 
 
 class JQFieldSpec:
-    """Typed dict for JQFieldSpec."""
+    """Typed dict for JQFieldSpec.
+
+    :param key_paths: The key paths
+    :type key_paths: :class:`Sequence`[`tuple`[`str`, ...]]
+    :param key_values: The key values
+    :type key_values: :class:`Sequence`[`tuple`[`str` |
+    :class:`None`, ...]]
+    :param value_paths: The value paths
+    :type value_paths: :class:`Sequence`[`tuple`[`str` |
+    :class:`None`, ...]]
+    :param value_type: The value type
+    :type value_type: :class:`Union`[`str`, `int`]
+    """
 
     def __init__(
         self,
-        key_paths: list[tuple[str, ...]],
-        key_values: list[tuple[str | None, ...]],
-        value_paths: list[tuple[str | None, ...]],
+        key_paths: Sequence[tuple[str, ...]],
+        key_values: Sequence[tuple[str | None, ...]],
+        value_paths: Sequence[tuple[str | None, ...]],
         value_type: Union[str, int],
     ) -> None:
+        """Constructor method."""
         self.validate_key_values_with_key_paths(key_values, key_paths)
         self.validate_value_paths_with_key_values(value_paths, key_values)
         self.key_paths = key_paths
@@ -54,9 +68,17 @@ class JQFieldSpec:
 
     @staticmethod
     def validate_key_values_with_key_paths(
-        key_values: list[tuple[str | None, ...]],
-        key_paths: list[tuple[str, ...]],
+        key_values: Sequence[tuple[str | None, ...]],
+        key_paths: Sequence[tuple[str, ...]],
     ) -> None:
+        """Validates key values with key paths.
+
+        :param key_values: The key values
+        :type key_values: :class:`Sequence`[`tuple`[`str` |
+        :class:`None`, ...]]
+        :param key_paths: The key paths
+        :type key_paths: :class:`Sequence`[`tuple`[`str`, ...]]
+        """
         if len(key_values) != len(key_paths):
             raise ValueError(
                 "Input key values and input key paths must have the same "
@@ -75,9 +97,18 @@ class JQFieldSpec:
 
     @staticmethod
     def validate_value_paths_with_key_values(
-        value_paths: list[tuple[str | None, ...]],
-        key_values: list[tuple[str | None, ...]],
+        value_paths: Sequence[tuple[str | None, ...]],
+        key_values: Sequence[tuple[str | None, ...]],
     ) -> None:
+        """Validates value paths with key values.
+
+        :param value_paths: The value paths
+        :type value_paths: :class:`Sequence`[`tuple`[`str` |
+        :class:`None`, ...]]
+        :param key_values: The key values
+        :type key_values: :class:`Sequence`[`tuple`[`str` |
+        :class:`None`, ...]]
+        """
         if len(value_paths) != len(key_values):
             raise ValueError(
                 "Input value paths and input key values must have the same "
@@ -103,79 +134,152 @@ class JQFieldSpec:
                     )
 
     @staticmethod
+    def field_spec_key_path_to_jq_key_path(
+        key_path: str | Iterable[str],
+    ) -> tuple[str, ...]:
+        """Converts field spec key path to jq key path.
+
+        :param key_path: The key path
+        :type key_path: `str` | `Iterable`[`str`]
+        :return: The jq key path
+        :rtype: `tuple`[`str`, ...]
+        """
+        if isinstance(key_path, str):
+            return (key_path,)
+        try:
+            priority_key_path = tuple(iter(key_path))
+            for key in priority_key_path:
+                if not isinstance(key, str):
+                    raise ValueError(
+                        "Priority key path, within an iterable, must be a "
+                        "string"
+                    )
+            return priority_key_path
+        except TypeError:
+            raise ValueError("Key path must be iterable or a string")
+
+    @staticmethod
     def field_spec_key_paths_to_jq_field_spec_key_paths(
-        key_paths: list[str | Iterable[str]],
+        key_paths: Sequence[str | Iterable[str]],
     ) -> list[tuple[str, ...]]:
+        """Converts field spec key paths to jq field spec key paths.
+
+        :param key_paths: The key paths
+        :type key_paths: :class:`Sequence`[`str` | `Iterable`[`str`]]
+        :return: The jq field spec key paths
+        :rtype: `list`[`tuple`[`str`, ...]]
+        """
         updated_key_paths: list[tuple[str, ...]] = []
         for key_path in key_paths:
-            if isinstance(key_path, str):
-                updated_key_paths.append((key_path,))
-            else:
-                try:
-                    priority_key_path = list(iter(key_path))
-                    for key in priority_key_path:
-                        if not isinstance(key, str):
-                            raise RuntimeError(
-                                "Priority key path, within an iterable, "
-                                "must be a string"
-                            )
-                    updated_key_paths.append(tuple(priority_key_path))
-                except TypeError:
-                    raise RuntimeError("Key path must be iterable or a string")
+            updated_key_paths.append(
+                JQFieldSpec.field_spec_key_path_to_jq_key_path(key_path)
+            )
         return updated_key_paths
 
     @staticmethod
-    def field_spec_optional_list_to_jq_field_spec_optional_list(
+    def optional_list_value_to_jq_optional_list_value(
+        optional_list_value: Optional[Union[str, Iterable[str | None]]],
+        jq_key_path: tuple[str, ...],
+    ) -> tuple[None, ...] | tuple[str | None, ...]:
+        """Converts optional list value to jq optional list value.
+
+        :param optional_list_value: The optional list value
+        :type optional_list_value: `Optional`[`Union`[`str`,
+        `Iterable`[`str` | `None`]]]
+        :param jq_key_path: The jq key path
+        :type jq_key_path: `tuple`[`str`, ...]
+        :return: The jq optional list value
+        :rtype: `tuple`[`str` | `None`, ...]
+        """
+        if optional_list_value is None:
+            return tuple([None] * len(jq_key_path))
+        elif isinstance(optional_list_value, str):
+            return (optional_list_value,)
+        else:
+            try:
+                priority_optional_list = tuple(iter(optional_list_value))
+                for key in priority_optional_list:
+                    if not isinstance(key, str) and key is not None:
+                        raise ValueError(
+                            "Priority key value, within an iterable, must be "
+                            "a string or None"
+                        )
+                return tuple(priority_optional_list)
+            except TypeError:
+                raise ValueError(
+                    "Key value must be iterable or a string"
+                )
+
+    @staticmethod
+    def optional_list_to_jq_optional_list(
         optional_list: Optional[
-            list[Optional[Union[str, Iterable[str | None]]]]
+            Sequence[Optional[Union[str, Iterable[str | None]]]]
         ],
-        jq_key_paths: list[tuple[str, ...]],
+        jq_key_paths: Sequence[tuple[str, ...]],
     ) -> list[tuple[str | None, ...]]:
+        """Converts optional list to jq optional list.
+
+        :param optional_list: The optional list
+        :type optional_list: `Optional`[`Sequence`[`Optional`[
+        `Union`[`str`, `Iterable`[`str` | `None`]]
+        ]]]
+        :param jq_key_paths: The jq key paths
+        :type jq_key_paths: `Sequence`[`tuple`[`str`, ...]]
+        :return: The jq optional list
+        :rtype: `list`[`tuple`[`str` | `None`, ...
+        ]]
+        """
         updated_optional_list: list[tuple[str | None, ...]] = []
         if optional_list is None:
-            for jq_key_path in jq_key_paths:
-                updated_optional_list.append(tuple([None] * len(jq_key_path)))
-            return updated_optional_list
+            optional_list = [None] * len(jq_key_paths)
         for key_value, key_path in zip(optional_list, jq_key_paths):
-            if key_value is None:
-                updated_optional_list.append(tuple([None] * len(key_path)))
-            elif isinstance(key_value, str):
-                updated_optional_list.append((key_value,))
-            else:
-                try:
-                    priority_optional_list = list(iter(key_value))
-                    updated_optional_list.append(tuple(priority_optional_list))
-                except TypeError:
-                    raise RuntimeError(
-                        "Key value must be iterable or a string"
-                    )
+            updated_optional_list.append(
+                JQFieldSpec.
+                optional_list_value_to_jq_optional_list_value(
+                    key_value, key_path
+                )
+            )
         return updated_optional_list
 
     @classmethod
     def from_field_spec(cls, field_spec: FieldSpec) -> Self:
+        """Converts from field spec.
+
+        :param field_spec: The field spec
+        :type field_spec: :class:`FieldSpec`
+        :return: The jq field spec
+        :rtype: :class:`JQFieldSpec`
+        """
         jq_field_spec_key_paths = (
             cls.field_spec_key_paths_to_jq_field_spec_key_paths(
                 field_spec["key_paths"]
             )
         )
         jq_field_spec_key_values = (
-            cls.field_spec_optional_list_to_jq_field_spec_optional_list(
+            cls.optional_list_to_jq_optional_list(
                 field_spec["key_value"], jq_field_spec_key_paths
             )
         )
-        jq_field_spec_key_values = (
-            cls.field_spec_optional_list_to_jq_field_spec_optional_list(
+        jq_field_spec_value_paths = (
+            cls.optional_list_to_jq_optional_list(
                 field_spec["value_paths"], jq_field_spec_key_paths
             )
         )
         return cls(
             key_paths=jq_field_spec_key_paths,
             key_values=jq_field_spec_key_values,
-            value_paths=jq_field_spec_key_values,
+            value_paths=jq_field_spec_value_paths,
             value_type=field_spec["value_type"],
         )
 
     def __eq__(self, value: object) -> bool:
+        """Equality method.
+
+        :param value: The value
+        :type value: `object`
+        :return: Whether the value is equal
+        :rtype: `bool`
+        """
         if not isinstance(value, JQFieldSpec):
             return False
         return (
