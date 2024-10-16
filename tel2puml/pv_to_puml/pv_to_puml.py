321f706b4c6142d4acd7242d6effa43823034a3c
Adrian Montagu <adrian.montagu@smartdcsit.co.uk>
2024-10-16 13:41:33 +0000

    195 fix import for flake8

--- tel2puml/pv_to_puml/pv_to_puml.py
+++ tel2puml/pv_to_puml/pv_to_puml.py
@@ -22,9 +22,9 @@ from tel2puml.pv_to_puml.walk_puml_graph.node_update import (
     update_nested_node_graph_with_break_points,
 )
 from tel2puml.loop_detection.detect_loops import detect_loops
-from tel2puml.pv_to_puml.walk_puml_graph.create_node_graph_from_event_graph import (
-    create_node_graph_from_event_graph,
-)
+from tel2puml.pv_to_puml.walk_puml_graph. \
+    create_node_graph_from_event_graph \
+    import create_node_graph_from_event_graph
 from tel2puml.pv_to_puml.walk_puml_graph.find_and_add_loop_kill_paths import (
     find_and_add_loop_kill_paths_to_nested_graphs,
 )
