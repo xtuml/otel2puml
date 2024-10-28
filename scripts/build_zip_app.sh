164f2f895d48223fbc67bb03f0c79a802026b85a
Freddie Mather <freddie.mather@smartdcs.co.uk>
2024-10-28 11:36:16 +0000

    259 - update script

--- scripts/build_zip_app.sh
+++ scripts/build_zip_app.sh
@@ -9,17 +9,12 @@ cd janus
 git fetch --all --tags
 git checkout tags/v1.0.0 -b latest
 cd ..
-cp -r janus/test_event_generator test_event_generator
+cp -r janus/test_event_generator tel2puml/test_event_generator
 
-mkdir -p dist
-
-# copy folders
-cp -r tel2puml dist/tel2puml
-cp -r test_event_generator dist/test_event_generator
 
 # install zipapps
 pip install zipapps
 
 # create zipapp
-python -m zipapps -c -u AUTO -a dist \
-    -m tel2puml.__main__ -o tel2puml_arm.pyz -r requirements.txt -r janus/requirements.txt
+python -m zipapps -c -u AUTO -a tel2puml \
+    -o tel2puml_arm.pyz -r requirements.txt -r janus/requirements.txt
